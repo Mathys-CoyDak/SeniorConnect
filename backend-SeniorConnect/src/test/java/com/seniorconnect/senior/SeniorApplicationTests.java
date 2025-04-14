7600ea467f12051b76279985a779b1aba7c4524a
Mathys-CoyDak <mathys.paillaugue@efrei.net>
2025-04-14 11:12:14 +0200

    innit commit

---
+++ backend-SeniorConnect/src/test/java/com/seniorconnect/senior/SeniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seniorconnect.senior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
