f44cc3405a46cb911d5fa8ece4d840acc94da6f7
Mathys-CoyDak <mathys.paillaugue@efrei.net>
2025-04-15 14:37:46 +0200

    Revert "Commit etape"
    This reverts commit 16e664c7b702a3debad420afb9f4c16a75f49a8e.

--- backend-SeniorConnect/src/main/java/com/portail/seniors/Senior/SeniorApplication.java
+++ backend-SeniorConnect/src/main/java/com/portail/seniors/Senior/SeniorApplication.java
@@ -1,4 +1,4 @@
-package com.seniorconnect.Senior;
+package com.portail.seniors.Senior;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend-SeniorConnect/src/main/java/com/seniorconnect/entity/TypeUtilisateur.java
+++
@@ -1,5 +0,0 @@
-package com.seniorconnect.entity;
-
-public enum TypeUtilisateur {
-    SENIOR, RECRUTEUR, ADMIN
-}
